5f83f8f8887b425e1d006f4bfe7b975bb234d90a
kanav <kanavraina98@gmail.com>
2020-10-29 18:17:11 +0530

    hacking work

---
+++ web/zone_transfer.sh
@@ -0,0 +1,4 @@
+host -t ns zonetransfer.me | cut -d " " -f 4       //zonetransfer.me is the url
+
+
+host -l zonetransfer.me nsztm1.digi.ninja         //nsztm1.digi.ninja is the server from the above command
